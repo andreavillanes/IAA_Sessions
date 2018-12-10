989e4d07ae1cdbe8f7b6e708f48c839963a08cf6
Dan Zaratsian <dtz001@gmail.com>
2018-12-10 10:15:37 -0500

    Create build.bat

---
+++ docker/build.bat
@@ -0,0 +1,5 @@
+docker network create dev
+docker build -t spark_240 containers/spark_240
+#docker build -t nifi containers/nifi
+#docker build -t phoenix containers/phoenix
+#docker build -t solr containers/solr
