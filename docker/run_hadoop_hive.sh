ce52e9c4b96ca48e8eca84db70092d7bfc3c62ef
Dan Zaratsian <dtz001@gmail.com>
2018-12-13 12:53:44 -0500

    Create run_hadoop_hive.sh

---
+++ docker/run_hadoop_hive.sh
@@ -0,0 +1,18 @@
+#####################################################################
+#
+#   https://github.com/big-data-europe/docker-hive
+#
+#   NOTE: Designed for, and tested on, linux
+#
+#####################################################################
+
+git clone https://github.com/big-data-europe/docker-hive.git
+
+cd docker-hive
+
+docker-compose up -d
+
+docker-compose exec hive-server bash
+
+
+#ZEND
