4871e7d73fe6920a1e4aed924851a2c531eb40ec
Dan Zaratsian <dtz001@gmail.com>
2018-12-28 10:06:20 -0500

    Update dataproc_1_create_cluster.sh

--- session_01/dataproc_1_create_cluster.sh
+++ session_01/dataproc_1_create_cluster.sh
@@ -5,12 +5,15 @@ PYSPARK_SCRIPT=spark_test_script.py
 gcloud dataproc clusters create $CLUSTER_NAME \
     --subnet default --zone us-east1-b \
     --master-machine-type n1-standard-2 --master-boot-disk-size 500 \
-    --num-workers 2 --worker-machine-type n1-standard-1 --worker-boot-disk-size 500 \
-    --num-preemptible-workers 2 \
+    --num-workers 3 --worker-machine-type n1-standard-2 --worker-boot-disk-size 500 \
+    --num-preemptible-workers 3 \
     --image-version 1.3-deb9 \
     --project zproject201807
 
 # Submit pyspark job
 #gcloud dataproc jobs submit pyspark --cluster=$CLUSTER_NAME $PYSPARK_SCRIPT
+
+# Cleanup / Remove Dataproc Cluster
+#gcloud dataproc clusters delete $CLUSTER_NAME
 
 #ZEND
