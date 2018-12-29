6c679eff33fee028198e4a24134f089e003a24a4
Dan Zaratsian <dtz001@gmail.com>
2018-12-29 13:55:39 +0000

    Updaded S1 and S2

--- docker/containers/hive/assets/hive_example_1_load_data.sh
+++ docker/containers/hive/assets/hive_example_1_load_data.sh
@@ -1,10 +1,12 @@
-su hdfs
 
-hadoop fs -mkdir /demo
-hadoop fs -mkdir /demo/ncaa
-hadoop fs -mkdir /demo/ncaa/SeasonResults
-hadoop fs -mkdir /demo/ncaa/Teams
-hadoop fs -put /tmp/SeasonResults.csv /demo/ncaa/SeasonResults/.
-hadoop fs -put /tmp/Teams.csv /demo/ncaa/Teams/.
+su -c "hadoop fs -mkdir /demo" hdfs
+su -c "hadoop fs -mkdir /demo/ncaa" hdfs
+su -c "hadoop fs -mkdir /demo/ncaa/SeasonResults" hdfs
+su -c "hadoop fs -mkdir /demo/ncaa/Teams" hdfs
+su -c "hadoop fs -put /assets/SeasonResults.csv /demo/ncaa/SeasonResults/." hdfs
+su -c "hadoop fs -put /assets/Teams.csv /demo/ncaa/Teams/." hdfs
 
-exit
+# Display hdfs directory
+su -c "hadoop fs -ls /demo/ncaa/" hdfs
+
+#ZEND
