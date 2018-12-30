dba74fdbc35b4722aca033859a0cd6fe6fd69934
Dan Zaratsian <dtz001@gmail.com>
2018-12-30 19:59:13 +0000

    Prep for S3

--- docker/spark_run.sh
+++ docker/spark_run.sh
@@ -3,14 +3,15 @@ docker rm -f spark
 
 docker run -it -d -p 18080:8080 -e SPARK_LOCAL_HOSTNAME=localhost --hostname spark --net dev --name spark spark
 
+# Start Zeppelin (running on localhost port 18080)
+docker exec spark /zeppelin/bin/zeppelin-daemon.sh start
+
 # Copy Assets
 docker cp containers/spark/assets spark:/.
 docker cp data spark:/.
 docker cp containers/spark/assets/notebook spark:/zeppelin/.
-docker cp containers/spark/assets/interpreter.json /zeppelin/conf/.
-
-# Start Zeppelin (running on localhost port 18080)
-docker exec spark /zeppelin/bin/zeppelin-daemon.sh start
+docker cp containers/spark/assets/interpreter.json spark:/zeppelin/conf/.
+docker cp containers/spark/assets/zeppelin-env.sh spark:/zeppelin/conf/.
 
 # Execute PySpark Test Script
 docker exec spark sh -c "/spark/bin/spark-submit /assets/spark_test_script.py" 
