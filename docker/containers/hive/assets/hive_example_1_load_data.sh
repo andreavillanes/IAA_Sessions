c4aaf8d441e0d89198b8588463210261ab99859c
Dan Zaratsian <dtz001@gmail.com>
2018-12-30 20:07:27 +0000

    misc cleanup for s1, s2, s3

--- docker/containers/hive/assets/hive_example_1_load_data.sh
+++ docker/containers/hive/assets/hive_example_1_load_data.sh
@@ -3,8 +3,8 @@ su -c "hadoop fs -mkdir /demo" hdfs
 su -c "hadoop fs -mkdir /demo/ncaa" hdfs
 su -c "hadoop fs -mkdir /demo/ncaa/SeasonResults" hdfs
 su -c "hadoop fs -mkdir /demo/ncaa/Teams" hdfs
-su -c "hadoop fs -put /assets/SeasonResults.csv /demo/ncaa/SeasonResults/." hdfs
-su -c "hadoop fs -put /assets/Teams.csv /demo/ncaa/Teams/." hdfs
+su -c "hadoop fs -put /data/SeasonResults.csv /demo/ncaa/SeasonResults/." hdfs
+su -c "hadoop fs -put /data/Teams.csv /demo/ncaa/Teams/." hdfs
 
 # Display hdfs directory
 su -c "hadoop fs -ls /demo/ncaa/" hdfs
