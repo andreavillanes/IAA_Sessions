f22806a5acd2ee4e0dd0e416d890cb0891d37118
Dan Zaratsian <dtz001@gmail.com>
2019-01-03 02:38:07 +0000

    Updated S2

--- docker/hive_run.sh
+++ docker/hive_run.sh
@@ -1,9 +1,12 @@
+docker stop hadoop-master
+docker rm hadoop-master
 
 # Run Docker container (Hadoop + Hive)
 docker run -it -d -p 50010:50010 -p 8020:8020 -p 8021:8021 -p 8088:8088 -p 10000:10000  --net dev --name hadoop-master --hostname hadoop-master teradatalabs/cdh5-hive
 
 # Copy Assets into Container
 docker cp containers/hive/assets/ hadoop-master:/
+docker cp data hadoop-master:/
 docker cp containers/hive/assets/connect_to_hive.sh hadoop-master:/.
 docker cp containers/hive/assets/hive_example_1_load_data.sh hadoop-master:/.
 docker cp containers/hive/assets/hive_example_2_query.sql hadoop-master:/.
